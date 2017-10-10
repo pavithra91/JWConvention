3b2d30db54a2c875b6cd2605fb8ca569ef903690
Pavithra - Home <Pavithra@DESKTOP-JDR9M7Q>
2017-10-11 00:54:25 +0530

    Home Commit

--- JWConvention/Controllers/ReservationController.cs
+++ JWConvention/Controllers/ReservationController.cs
@@ -11,100 +11,51 @@ namespace JWConvention.Controllers
     {
         wlakerstoursdbEntities _context = new wlakerstoursdbEntities();
         // GET: Reservation
+        public ActionResult Registration(string HotelCode)
+        {
+            ReservationModel objModel = new ReservationModel();
+
+            JW_BookingID _tempBookingID = new JW_BookingID();
+            _context.JW_BookingID.Add(_tempBookingID);
+            _context.SaveChanges();
+
+            _tempBookingID.BookingID = "JW" + _tempBookingID.AUTOID;
+            _context.Entry(_tempBookingID).State = System.Data.Entity.EntityState.Modified;
+            _context.SaveChanges();
+
+            objModel.BookingID = _tempBookingID.BookingID;
+            objModel._additionalList = new List<AddtionalAccommodation>();
+
+            objModel._hotelCode = HotelCode;
+            objModel._delegate = new DelegateModel();
+
+            TempData["ReservationModel"] = objModel;
+            return View(objModel);
+        }
+
         public ActionResult Index(string HotelCode)
         {
             try
             {
                 //ReservationModel objModel = new ReservationModel(); 
                 ReservationModel objModel = TempData["ReservationModel"] as ReservationModel;
-                if(objModel==null)
-                {
-                    ReservationModel objModel1 = new ReservationModel();
+                objModel._roomList = new List<JW_Rooms>();
+                objModel._hotelList = new List<JW_Hotels>();
 
-                    JW_BookingID _tempBookingID = new JW_BookingID();
-                    _context.JW_BookingID.Add(_tempBookingID);
-                    _context.SaveChanges();
+                JW_Hotels _hotel = _context.JW_Hotels.Where(w => w.HotelCode == HotelCode).FirstOrDefault();
+                objModel._hotel = _hotel;
+                objModel._hotelCode = HotelCode;
 
-                    _tempBookingID.BookingID = "JW" + _tempBookingID.AUTOID;
-                    _context.Entry(_tempBookingID).State = System.Data.Entity.EntityState.Modified;
-                    _context.SaveChanges();
-
-                    objModel1.BookingID = _tempBookingID.BookingID;
-                    objModel1._additionalList = new List<AddtionalAccommodation>();
-
-                    JW_Hotels _hotel = _context.JW_Hotels.Where(w => w.HotelCode == HotelCode).FirstOrDefault();
-                    objModel1._hotel = _hotel;
-                    objModel1._roomList = new List<JW_Rooms>();
-                    objModel1._hotelList = new List<JW_Hotels>();
-
-                    objModel1._roomList = _context.JW_Rooms.Where(w => w.HotelID == _hotel.HotelID).ToList();
-                    objModel1._hotelList = _context.JW_Hotels.ToList();
-                    return View(objModel1);
-                }
-                else
-                {
-                    objModel._roomList = new List<JW_Rooms>();
-                    objModel._hotelList = new List<JW_Hotels>();
-
-                    JW_Hotels _hotel = _context.JW_Hotels.Where(w => w.HotelCode == HotelCode).FirstOrDefault();
-                    objModel._hotel = _hotel;
-
-                    objModel._roomList = _context.JW_Rooms.Where(w => w.HotelID == _hotel.HotelID).ToList();
-                    objModel._hotelList = _context.JW_Hotels.ToList();
-
-                    // = _context.JW_TempReservation.Where(w=>w.BookingID == objModel.BookingID).ToList()
-                    return View(objModel);
-                }
+                objModel._roomList = _context.JW_Rooms.Where(w => w.HotelID == _hotel.HotelID).ToList();
+                objModel._hotelList = _context.JW_Hotels.ToList();
                 
+                return View(objModel); 
             }
             catch(Exception ex)
             {
                 return null;
             }
         }
-
-        [HttpPost]
-        public ActionResult SaveAdditionalRoom(ReservationModel ObjModel)
-        {
-            ReservationModel tempModel = TempData["ReservationModel"] as ReservationModel;
-
-            var bookingId = _context.JW_BookingID.Where(w => w.BookingID == tempModel.BookingID).FirstOrDefault();
-
-            if(bookingId!=null)
-            {
-                JW_TempReservation _temp = new JW_TempReservation();
-                _temp.BookingID = bookingId.BookingID;
-                _temp.AdditionalHotel = ObjModel._additional._addtionalHotel;
-                _temp.AdditionalRoom = ObjModel._additional._addtionalRoom;
-                _temp.AdditionalOccupancy = ObjModel._additional._addtionalOccupancy;
-                //_temp.AdditionalDate = ObjModel._additional._addtionalDate;
-                _temp.AdditionalNoofRooms = ObjModel._additional._addtionalNoOfRooms;
-                _temp.AdditionalNoofNights = ObjModel._additional._addtionalNoOfNights;
-
-                _context.JW_TempReservation.Add(_temp);
-                _context.SaveChanges();
-
-                if(tempModel._additionalList == null)
-                {
-                    tempModel._additionalList = new List<AddtionalAccommodation>();
-                }
-
-                tempModel._additionalList.Add(ObjModel._additional);
-
-                TempData["ReservationModel"] = tempModel;
-            }            
-
-            return RedirectToAction("AddtionalAccomerdation", "Reservation");
-        }
-
-        [HttpPost]
-        public ActionResult SaveReservation(ReservationModel ObjModel)
-        {
-            TempData["ReservationModel"] = ObjModel;
-
-            return RedirectToAction("AddtionalAccomerdation", "Reservation");
-        }
-
 
         public ActionResult AddtionalAccomerdation()
         {
@@ -119,20 +70,65 @@ namespace JWConvention.Controllers
         }
 
         [HttpPost]
-        public ActionResult Summery()
+        public ActionResult SaveAdditionalRoom(ReservationModel ObjModel)
+        {
+            ReservationModel tempModel = TempData["ReservationModel"] as ReservationModel;
+
+            var bookingId = _context.JW_BookingID.Where(w => w.BookingID == tempModel.BookingID).FirstOrDefault();
+
+            if (bookingId != null)
+            {
+                JW_TempReservation _temp = new JW_TempReservation();
+                _temp.BookingID = bookingId.BookingID;
+                _temp.AdditionalHotel = ObjModel._additional._addtionalHotel;
+                _temp.AdditionalRoom = ObjModel._additional._addtionalRoom;
+                _temp.AdditionalOccupancy = ObjModel._additional._addtionalOccupancy;
+                //_temp.AdditionalDate = ObjModel._additional._addtionalDate;
+                _temp.AdditionalNoofRooms = ObjModel._additional._addtionalNoOfRooms;
+                _temp.AdditionalNoofNights = ObjModel._additional._addtionalNoOfNights;
+
+                _context.JW_TempReservation.Add(_temp);
+                _context.SaveChanges();
+
+                if (tempModel._additionalList == null)
+                {
+                    tempModel._additionalList = new List<AddtionalAccommodation>();
+                }
+
+                tempModel._additionalList.Add(ObjModel._additional);
+
+                JW_Hotels _hotel = _context.JW_Hotels.Where(w => w.HotelCode == ObjModel._hotelCode).FirstOrDefault();
+                tempModel._hotel = _hotel;
+                tempModel._hotelCode = ObjModel._hotelCode;
+                tempModel._delegate = ObjModel._delegate;
+
+                TempData["ReservationModel"] = tempModel;
+            }
+
+            return RedirectToAction("AddtionalAccomerdation", "Reservation");
+        }
+
+        [HttpPost]
+        public ActionResult SaveReservation(ReservationModel ObjModel)
+        {
+            TempData["ReservationModel"] = ObjModel;
+
+            return RedirectToAction("AddtionalAccomerdation", "Reservation");
+        }
+
+        [HttpPost]
+        public ActionResult PostSummery()
         {
             ReservationModel objModel = TempData["ReservationModel"] as ReservationModel;
 
-            return View(objModel);
+            TempData["ReservationModel"] = objModel;
+
+            return RedirectToAction("Summery", "Reservation");
         }
 
-        public ActionResult Registration(string HotelCode)
+        public ActionResult Summery()
         {
-            ReservationModel objModel = new ReservationModel();
-            objModel._hotelCode = HotelCode;
-            objModel._delegate = new DelegateModel();
-
-            TempData["ReservationModel"] = objModel;
+            ReservationModel objModel = TempData["ReservationModel"] as ReservationModel;
             return View(objModel);
         }
 
